48122106ff645c9bc9c4c8be4ebd9b0a5e510ee4
kukaro <justkukaro@naver.com>
2021-10-27 01:12:58 +0900

    refactor: 269p3

--- assignment/269p3.py
+++ assignment/269p3.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 diabetes = pd.read_csv('../data/pima2.csv')
+result = {}
 
-print(diabetes.attrs())
-print(diabetes)
+for col in diabetes.columns:
+    freq_table = pd.crosstab(index=diabetes[col], colnames=[col.upper()], columns='count')
+    # 비율은 백분율입니다.
+    freq_table['rate'] = freq_table / sum(freq_table['count']) * 100
+    result[col] = freq_table
+
+for _, col_value in result.items():
+    plt.bar(col_value.index, col_value['count'])
+    plt.show()
